e580f24ec1150fac8e96d58330ab16054de2bcf3
frankcr2207 <frankcr2207@gmail.com>
2024-11-18 22:55:50 -0500

    feat: base proyecto

---
+++ src/main/java/csjar/controlpatrimonial/ControlpatrimonialApplication.java
@@ -0,0 +1,13 @@
+package csjar.controlpatrimonial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlpatrimonialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlpatrimonialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csjar/controlpatrimonial/ControlpatrimonialApplicationTests.java
@@ -0,0 +1,13 @@
+package csjar.controlpatrimonial;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlpatrimonialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
